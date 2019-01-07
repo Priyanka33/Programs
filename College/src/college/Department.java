2364b71cb8aad58865d8a5cd85ded0607697708d
Priyanka33 <34092816+Priyanka33@users.noreply.github.com>
2019-01-07 13:40:37 +0530

    Update Department.java

--- College/src/college/Department.java
+++ College/src/college/Department.java
@@ -4,20 +4,29 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Department {
 
     String departmentname;
     ArrayList<Student> students = new ArrayList<>();
+
     ArrayList<Student> createStudent() throws IOException, ParseException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         for (int i = 0; i < 3; i++) {
             Student student = new Student();
             System.out.println("enter Student name");
             student.studentname = in.readLine();
-            System.out.println("Enter DoB");
-            student.dob = Integer.parseInt(in.readLine());
+            try {
+                System.out.print("ENTER dob  (mm/dd/yyyy ): ");
+                String dob = in.readLine();
+                Date date = new SimpleDateFormat("mm/dd/yyyy").parse(dob);
+            } catch (Exception e) {
+
+            }
+
             System.out.println("enter Email");
             student.email = in.readLine();
             students.add(student);

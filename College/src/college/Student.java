19a82add5df9f158d3e1225e5bb395200e695dc9
Priyanka33 <34092816+Priyanka33@users.noreply.github.com>
2019-01-07 13:41:51 +0530

    Update Student.java

--- College/src/college/Student.java
+++ College/src/college/Student.java
@@ -1,13 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package college;
 
+import java.util.Date;
+
 public class Student {
-    
+
     String studentname;
-    int dob;
+    Date dob;
     String email;
 }
